46caad4e3b4784d1daf3f870213d3581e2bfbc0e
matheusabporto <matheusabporto@gmail.com>
2026-03-24 11:17:24 -0300

    Add files via upload

---
+++ Exercicios/elevadorPredio/Elevador/src/Elevador.java
@@ -0,0 +1,15 @@
+public class Elevador {
+/*
+Exercicio. Criar uma classe denominada Elevador para armazenar as informacoes de um elevador dentro de um predio. A classe deve armazenar o
+andar atual (terreo = 0), total de andares no predio, capacidade do elevador
+(numero maximo de pessoas) e quantas pessoas estao presentes nele. Alem
+de metodos gets e sets, a classe deve disponibilizar os seguintes componentes:
+• construtor: que deve receber como parametros a capacidade do elevador
+e o total de andares no predio (os elevadores sempre comecam no terreo e vazio);
+• entra: para acrescentar uma pessoa no elevador (so deve acrescentar sem ainda houver espaco);
+• sai: para remover uma pessoa do elevador (so deve remover se houver alguem dentro dele);
+• sobe: para subir um andar (nao deve subir se ja estiver no ultimo andar);
+• desce: para descer um andar (nao deve descer se ja estiver no terreo).
+*/
+
+}
