e61220898134beb4d8e2cd2b11e01642438b9d1e
matheusabporto <matheusabporto@gmail.com>
2026-05-05 14:34:20 -0300

    adicionando ex.ProdutoeLoja

--- Exercicios/exProdutoeLoja/src/App.java
+++ Exercicios/exProdutoeLoja/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Produto produto1 = new Produto("1", "Camiseta", 50.0, 10);
+        Loja loja1 = new Loja(produto1);
+
+        loja1.valorEstoque(); // Valor total do estoque: 500.0
+        loja1.vender(2); // Venda realizada!
+        loja1.valorEstoque(); // Valor total do estoque: 400.0
+        loja1.abastecer(5); // Abastecimento realizado!
+        loja1.valorEstoque(); // Valor total do estoque: 650.0
     }
 }
--- Exercicios/exProdutoeLoja/src/Loja.java
+++ Exercicios/exProdutoeLoja/src/Loja.java
@@ -1,3 +1,44 @@
 public class Loja {
 
+    /*
+Implementar a classe `Loja` com conjunto de produtos. 
+ Métodos: `vender()` (realiza a venda de uma unidade); `abastecer()` (abastece uma unidade); 
+ `valorEstoque()` (computa o valor total do estoque). Instanciar um objeto `Loja` e fazer chamadas.
+*/
+
+//atributos
+Produto produto;
+
+//construtor
+
+public Loja(Produto produto){
+    this.produto = produto;
+}
+
+//getters e setters
+
+public Produto getProduto(){
+    return this.produto;
+}
+public void setProduto(Produto produto){
+    this.produto = produto;
+}
+
+//metodos
+
+public void vender(int quantidade){
+    this.produto.setQtdEstoque(this.produto.getQtdEstoque() - quantidade);
+    System.out.println("Venda realizada!");
+}
+
+public void abastecer(int quantidade){
+    this.produto.setQtdEstoque(this.produto.getQtdEstoque() + quantidade);
+    System.out.println("Abastecimento realizado!");
+}
+
+public double valorEstoque(){
+    System.out.println("Valor total do estoque: " + (this.produto.getValor() * this.produto.getQtdEstoque()));
+    return (this.produto.getValor() * this.produto.getQtdEstoque());
+}
+
 }
